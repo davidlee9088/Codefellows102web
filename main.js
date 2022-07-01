bfa1ca2be991aa404e3b7fee6c4bd6f56f2f20a4
Hanbyeol <davidlee9088@gmail.com>
2022-06-30 19:05:14 -0700

    updated2

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-let favcountry = "korea"
-let favcountry2 = "japan"
-let favcountry3 = "china"
-let userfav = prompt("Please enter your country to visit","Any country")
-userfav = userfav.toLowerCase()
-if (userfav == favcountry || userfav == favcountry2 || userfav == favcountry3) {
-    alert("THEN YOU SHOULD GO right now")
-} else {
-    alert(userfav + " that country is great, but please try SK,JP,CN")
-} 
 
-function myFunction() 
-{
-    let test1 = Math.floor(Math.random() *3 ) 
-    
-    if (test1 == 0) {
-        document.getElementById("demo").innerHTML = ("Korea")
-    } else if (test1 == 1) {
-    document.getElementById("demo").innerHTML = ("Japan")
+function UserC() {
+    let favcountry = "korea";
+    let favcountry2 = "japan";
+    let favcountry3 = "china";
+    let input = prompt("Please enter your country to visit", "Any country");
+    input = input.toLowerCase();
+    if (input == favcountry || input == favcountry2 || input == favcountry3) {
+        alert("THEN YOU SHOULD GO right now");
     } else {
-    document.getElementById("demo").innerHTML = ("China")
-    }
+        alert(input + " that country is great, but please try SK,JP,CN");
+    };
 
 }
+UserC();
+function RandomCoun() {
+    let test1 = Math.floor(Math.random() * 3);
+    if (test1 == 0) {
+        document.getElementById("demo").innerHTML = ("Korea");
+    } else if (test1 == 1) {
+        document.getElementById("demo").innerHTML = ("Japan");
+    } else {
+        document.getElementById("demo").innerHTML = ("China");
+    };
 
-function myFunction2()
-{
-    document.getElementById("demo2").innerHTML = ("Because they are fun countries!")
 }
+function showPic() {
+    const userNum = prompt("How many cat pictures do you want")
+
+    for (let i = 0; i < userNum; i++) {
+        document.write("<img src='https://placekitten.com/640/360'></img>")
+
+
+    };
+}
+
+
+function ReasonGo() {
+    document.getElementById("demo2").innerHTML = "Because they are fun countries!";
+}
+
